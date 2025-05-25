cbb8ee5e79ff80daba2f4d7282e89aa9b135d98d
yana <76143818+needstosleepmore@users.noreply.github.com>
2025-05-25 00:07:22 -0500

    heh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int num = 100;
+        while (num > 3){
+            System.out.println("this is the " + num + "th time");
+            num--;
+        }
     }
 }
