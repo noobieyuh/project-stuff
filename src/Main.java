0e3639010ce8b9172aa83373845b239cccc047ab
yana <76143818+needstosleepmore@users.noreply.github.com>
2025-05-24 23:57:51 -0500

    more and more changes to main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hallo");
         System.out.println("whats up");
+        System.out.println("bleh");
     }
 }
